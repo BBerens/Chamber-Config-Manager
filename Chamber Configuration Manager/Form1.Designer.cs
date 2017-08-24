256f08207102efc19a3d351cf8ba55abe06c3ca9
bberens <brett.s.berens@gmail.com>
2017-08-24 00:44:57 -0700

    read from default xml complete. Work on search

--- Chamber Configuration Manager/Form1.Designer.cs
+++ Chamber Configuration Manager/Form1.Designer.cs
@@ -28,18 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(197, 89);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(674, 70);
-            this.textBox1.TabIndex = 0;
             // 
             // dataGridView1
             // 
@@ -49,13 +56,174 @@
             this.dataGridView1.Size = new System.Drawing.Size(956, 330);
             this.dataGridView1.TabIndex = 1;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Select a Configuration";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(87, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(132, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(237, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(565, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(66, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(66, 66);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(66, 93);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(237, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Enabled = false;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(87, 89);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(132, 21);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(66, 120);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(15, 14);
+            this.checkBox4.TabIndex = 14;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(237, 117);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(150, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Enabled = false;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(87, 116);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(132, 21);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Enabled = false;
+            this.checkBox5.Location = new System.Drawing.Point(66, 147);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(15, 14);
+            this.checkBox5.TabIndex = 17;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(237, 144);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(150, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Enabled = false;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(87, 143);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(132, 21);
+            this.comboBox5.TabIndex = 15;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Enabled = false;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(237, 36);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(132, 21);
+            this.comboBox6.TabIndex = 18;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1050, 547);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -66,8 +234,23 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
     }
 }
 
--- Chamber Configuration Manager/Form1.cs
+++ Chamber Configuration Manager/Form1.cs
@@ -14,40 +14,35 @@ namespace Chamber_Configuration_Manager
 {
     public partial class Form1 : Form
     {
+        List<ChamberConfig> chamberList = new List<ChamberConfig>();
+        List<List<string>> parameterList = new List<List<string>>();
+
         public Form1()
         {
-            XmlDocument Document = new XmlDocument();
-            XmlNode root, chamberNode;
-            List<ChamberConfig> chamberList = new List<ChamberConfig>();
-            ChamberConfig newChamber;
-            IEnumerator chamberEnum;
+                        
             DataRow row;
-            Document.Load("C:\\Users\\DrStrange\\Source\\Repos\\Chamber-Config-Manager\\Chamber Configuration Manager\\Chambers.xml");
-            root = Document.DocumentElement;
-            chamberEnum = root.GetEnumerator();
+            List<string> columnAttributes = new List<string>();
+            importParse("C:\\Users\\DrStrange\\Source\\Repos\\Chamber-Config-Manager\\Chamber Configuration Manager\\Chamber Configurations.xml", chamberList, parameterList); 
+            
+ 
             InitializeComponent();
-            while(chamberEnum.MoveNext())
+
+            foreach (string[] attribute in chamberList[0].chamberAttributes)
+                columnAttributes.Add(attribute[0]);
+
+            DataTable dataTable = new DataTable("All Chambers");
+            DataTable searchResults = new DataTable("Search Results");
+            foreach( string attribute in columnAttributes)
             {
-                chamberNode = (XmlNode)chamberEnum.Current;
-                newChamber = new ChamberConfig(chamberNode);
-                chamberList.Add(newChamber);
+                dataTable.Columns.Add(attribute, typeof(string));
+                searchResults.Columns.Add(attribute, typeof(string));
             }
 
-            DataTable dataTable = new DataTable("Search Results");
-            dataTable.Columns.Add("Chamber Id", typeof(string));
-            dataTable.Columns.Add("Tool Id", typeof(string));
-            dataTable.Columns.Add("Chamber Location", typeof(string));
-            foreach( string[] attribute in chamberList[0].chamberAttributes)
-            {
-                dataTable.Columns.Add(attribute[0], typeof(string));
-            }
+
             dataGridView1.DataSource = dataTable;
             foreach(ChamberConfig chamber in chamberList)
             {
                 row = dataTable.NewRow();
-                row["Chamber Id"] = chamber.chamberId;
-                row["Tool Id"] = chamber.toolId;
-                row["Chamber Location"] = chamber.chamberLoc;
                 foreach(string[] attribute in chamber.chamberAttributes)
                 {
                     row[attribute[0]] = attribute[1];
@@ -55,6 +50,53 @@ namespace Chamber_Configuration_Manager
                 dataTable.Rows.Add(row);
             }
             dataGridView1.Refresh();
+            comboBox1.DataSource = columnAttributes;
+        }
+
+        private void importParse(string filepath, List<ChamberConfig> chamberList, List<List<string>> parameterList)
+        {
+            XmlDocument importFile = new XmlDocument();
+            XmlNode root, node, child, chamberNode;
+            IEnumerator chamberEnum, paramEnum, valueEnum;
+            ChamberConfig newChamber;
+            List<string> paramValueList;
+
+            importFile.Load(filepath);
+            root = importFile.DocumentElement;
+            paramEnum = root["Parameters"].GetEnumerator();
+            while(paramEnum.MoveNext())
+            {
+                paramValueList = new List<string>();
+                node = (XmlNode)paramEnum.Current;
+                paramValueList.Add(node.Attributes["name"].Value);
+                valueEnum = node.FirstChild.GetEnumerator();
+                while(valueEnum.MoveNext())
+                {
+                    child = (XmlNode)valueEnum.Current;
+                    paramValueList.Add(child.InnerText);
+                }
+                parameterList.Add(paramValueList);
+            }
+
+            chamberEnum = root["Testset"].GetEnumerator();
+            while (chamberEnum.MoveNext())
+            {
+                chamberNode = (XmlNode)chamberEnum.Current;
+                newChamber = new ChamberConfig(chamberNode, parameterList);
+                chamberList.Add(newChamber);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox6.Enabled = true;
+            List<string> sublist = parameterList[comboBox1.SelectedIndex].GetRange(1, parameterList[comboBox1.SelectedIndex].Count - 1);
+            comboBox6.DataSource = sublist;
         }
     }
 }
